6163a4770792ed7a87c2531abb91be3ca8c8c111
Farshd-Forutan <Farshad.Forutan.2001@gmail.com>
2025-12-27 00:36:36 +0330

    update: docker-compose updated for payment and some fixes
    - payment -service configuration added to docker-compose
    - JWT added to index.js
    - packages had high level vulnerability which fixed through version change

--- services/payment-service/src/config/index.js
+++ services/payment-service/src/config/index.js
@@ -5,6 +5,9 @@ module.exports = {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || "development",
   },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
   db: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
