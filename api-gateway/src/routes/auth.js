494060dfbe7a253a342d8356ef02d8522ba13d04
AtaAP <aata365@yahoo.com>
2025-12-26 11:48:37 +0330

    feat(api-gateway): implement auth & product routing with JWT middleware

---
+++ api-gateway/src/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const axios = require("axios");
+
+const router = express.Router();
+
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
+
+// signup
+router.post("/signup", async (req, res) => {
+  try {
+    const response = await axios.post(
+      `${USER_SERVICE_URL}/api/auth/signup`,
+      req.body
+    );
+    res.json(response.data);
+  } catch (err) {
+    res
+      .status(err.response?.status || 500)
+      .json(err.response?.data || { error: "User service error" });
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const response = await axios.post(
+      `${USER_SERVICE_URL}/api/auth/login`,
+      req.body
+    );
+    res.json(response.data);
+  } catch (err) {
+    res
+      .status(err.response?.status || 500)
+      .json(err.response?.data || { error: "User service error" });
+  }
+});
+
+module.exports = router;
